acbf5cb37eed5d68798c2badacc63e8729fd2b4f
TalOmari <talomari592@gmail.com>
2019-09-11 13:02:22 +0200

    set up for deploying

---
+++ Server-side/build/precache-manifest.ebf5b377c9ae2bc82e3be82f1f44742d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32afa4b48228e3017d63ebf8ec117be2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "319391fba24aa3fbad10",
+    "url": "/static/css/main.8c34ee8e.chunk.css"
+  },
+  {
+    "revision": "09e1bfb63519b0b096a2",
+    "url": "/static/js/2.15c339d4.chunk.js"
+  },
+  {
+    "revision": "319391fba24aa3fbad10",
+    "url": "/static/js/main.46e0e50a.chunk.js"
+  },
+  {
+    "revision": "8ed6144ee1801e4e92c6",
+    "url": "/static/js/runtime~main.74c2a6e3.js"
+  },
+  {
+    "revision": "8a9f3768d862d635a48506fbde8fefbd",
+    "url": "/static/media/logo.8a9f3768.png"
+  },
+  {
+    "revision": "b1361fd846c369a4555f84fc28abae16",
+    "url": "/static/media/pic11.b1361fd8.jpg"
+  },
+  {
+    "revision": "a54514191799b1c13448a222b190f5e8",
+    "url": "/static/media/pic3.a5451419.jpg"
+  }
+]);
