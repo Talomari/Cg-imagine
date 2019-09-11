dd32f1b2ad91a5f7e95ea7a10e6a221eb988d16b
TalOmari <talomari592@gmail.com>
2019-09-11 18:59:15 +0300

    deploying

--- client-side/src/api.js
+++ client-side/src/api.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios'
 const Axios = axios.create({
-    baseURL: 'http://localhost:8080/',
+    baseURL: '',
     headers: {
         'Access-Control-Allow-Origin': '*'
     }
